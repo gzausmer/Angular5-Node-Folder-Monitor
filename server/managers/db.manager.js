88c26cf91f3dd972c249bb39fe1ab41fbe87173e
Zausmer <Gil.Zausmer@verint.com>
2018-02-05 18:25:25 +0200

    db creation modification

--- server/managers/db.manager.js
+++ server/managers/db.manager.js
@@ -8,31 +8,17 @@ class MyDBManager {
                 return console.error(err.message);
             }
             console.log('Connected to the myDB2 SQlite database.');
-            db.run("CREATE TABLE if not exists users(user_id integer PRIMARY KEY, user_name text NOT NULL, password text NOT NULL)");
-            db.close((err) => {
-                if (err) {
-                    return console.error(err.message);
-                }
-                console.log('Close the database connection.');
-            });
-            this.createTable();
-        });
-    }
-    createTable() {
-        // just for demo purposes - I added passwords to the db without going through hash functions..
-        const db = new sqlite3.Database('myDB2', (err) => {
-            if (err) {
-                return console.error(err.message);
-            }
-            console.log('Connected to the myDB2 SQlite database.');
-            db.run("DELETE FROM users");
-            db.run("INSERT into users(user_id,user_name,password) VALUES (2,'moroder','pass')");
-            db.run("INSERT into users(user_id,user_name,password) VALUES (3,'bla','wordd')");
-            db.close((err) => {
-                if (err) {
-                    return console.error(err.message);
-                }
-                console.log('Close the database connection.');
+            db.serialize(() => {
+                db.run("CREATE TABLE if not exists users(user_id integer PRIMARY KEY, user_name text NOT NULL, password text NOT NULL)");
+                db.run("DELETE FROM users");
+                db.run("INSERT into users(user_id,user_name,password) VALUES (2,'moroder','pass')");
+                db.run("INSERT into users(user_id,user_name,password) VALUES (3,'bla','wordd')");
+                db.close((err) => {
+                    if (err) {
+                        return console.error(err.message);
+                    }
+                    console.log('Close the database connection.');
+                });
             });
         });
     }
